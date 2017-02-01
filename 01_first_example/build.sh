ffea5f68591669019bddccf4575639d35b56eaa6
Kulbhushan Singhal <kulbhushan.singhal@plusgrade.com>
2017-02-01 11:19:00 +0530

    Java 9 modularization 1st example

--- 01_first_example/build.sh
+++ 01_first_example/build.sh
@@ -5,11 +5,11 @@ rm -rf out/lib
 mkdir -p out/lib
 
 rm -rf out/greetings.out
-javac -d out/greetings.out `find src/com.greetings -name "*.java"`
+javac -d out/greetings.out `find com.greetings -name "*.java"`
 jar  -c -f out/lib/greetings.jar -C out/greetings.out .
 
 rm -rf out/user.out
-javac -p out/lib -d out/user.out `find src/greetings.user -name "*.java"`
+javac -p out/lib -d out/user.out `find greetings.user -name "*.java"`
 jar  -c -f out/lib/user.jar --main-class=com.greetings.user.User -C out/user.out .
 
 
---
+++ README.md
@@ -0,0 +1,1 @@
+Examples to demonstrate the modularity changes coming in Java 9 due to be released in July 2017
